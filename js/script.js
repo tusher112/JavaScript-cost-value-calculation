2367548df6c73d7a4a57fe8d20dd7dfb0fc12494
tusher112 <mdtusherahmed820@gmail.com>
2022-02-17 13:10:47 +0600

    input function & dashboard balance  added

--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
+//common function
+function getInput(value) {
+    let InputFiled = parseInt(document.getElementById(value).value);
+    if (InputFiled < 0) {
+        alert("Please Provide a positive number");
+        InputFiled = 0;
+
+    }
+    else if (InputFiled >= 0) {
+        return InputFiled;
+    }
+    return "enter right amount";
+}
+
+function dashBoard(value) {
+    const valueOfDashboard = document.getElementById(value);
+    return valueOfDashboard;
+}
+
+
+
+
 //calculate button's function
 document.getElementById('calculate-button').addEventListener('click', function () {
+    const incomeInputFiled = getInput('income-input');
+    const foodInputFiled = getInput('food-cost');
+    const rentInputFiled = getInput('rent-cost');
+    const clothesInputFiled = getInput('clothes-cost');
+    let totalExpensesDashboard = dashBoard('total-expenses-dashboard');
+    totalExpensesDashboard.innerText = parseInt((isNaN(foodInputFiled) ? 0 : +foodInputFiled) + (isNaN(rentInputFiled) ? 0 : +rentInputFiled) + (isNaN(clothesInputFiled) ? 0 : +clothesInputFiled));
 
 
-    const incomeInputFiled = parseInt(document.getElementById('income-input').value);
-
-    const foodInputFiled = parseInt(document.getElementById('food-cost').value);
-    const rentInputFiled = parseInt(document.getElementById('rent-cost').value);
-    const clothesInputFiled = parseInt(document.getElementById('clothes-cost').value);
-
-    let totalExpensesDashboard = document.getElementById('total-expenses-dashboard');
-    totalExpensesDashboard.innerText = parseInt(foodInputFiled + rentInputFiled + clothesInputFiled);
-
-    let balanceDashboard = document.getElementById('total-balance-dashboard');
+    let balanceDashboard = dashBoard('total-balance-dashboard');
     balanceDashboard.innerText = incomeInputFiled - totalExpensesDashboard.innerText;
 
-    let remainingBalance = document.getElementById('remaining-balance-dashboard');
+    let remainingBalance = dashBoard('remaining-balance-dashboard');
     remainingBalance.innerText = balanceDashboard.innerText;
 
 })
